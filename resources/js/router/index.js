1e8821a7ac574b7112408ce91c993d87c94cd652
Jesse Merritt <jmerrittdev@gmail.com>
2021-04-04 16:06:11 -0400

    Remove outdated references to routes

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,29 +1,11 @@
 import { createWebHistory, createRouter } from "vue-router";
 const Home = () => import('@/views/ViewHome.vue');
-const About = () => import('@/views/ViewAbout.vue');
-const Portfolio = () => import('@/views/ViewPortfolio.vue');
-const Contact = () => import('@/views/ViewContact.vue');
 
 const routes = [
     {
         path: "/",
         name: "Home",
         component: Home,
-    },
-    {
-        path: "/about",
-        name: "About",
-        component: About,
-    },
-    {
-        path: "/portfolio",
-        name: "Portfolio",
-        component: Portfolio,
-    },
-    {
-        path: "/contact",
-        name: "Contact",
-        component: Contact,
     },
 ];
 
