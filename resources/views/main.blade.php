6f62a95b7b90c1b650f9156428977a2ce34112f2
Jesse Merritt <jmerrittdev@gmail.com>
2021-04-16 23:04:40 -0400

    Add cache busting for app.js

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Jesse Merritt</title>
-        <script defer src="js/app.js"></script>
+        <script defer src="{{ mix('/js/app.js') }}"></script>
     </head>
     <body>
         <div id="app"></div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -38,3 +38,7 @@ mix.js('resources/js/app.js', 'public/js')
         open: false,
     })
     .vue();
+
+if (mix.inProduction()) {
+    mix.version();
+}
